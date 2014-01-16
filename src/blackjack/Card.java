f2f740cd99b0d032925fac7779d514706c2dfcf9
Karen Clark <kiroons@gmail.com>
2014-01-16 17:10:16 -0600

    Added build to gitignore. Added logic to initialize/shuffle card deck.

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -9,6 +9,16 @@ public class Card {
     
     private Suit suit;
     private Rank rank;
+    
+    /**
+     * Constructor
+     * @param s the suit of the card (e.g., Heart, Diamond)
+     * @param r the rank of the card (e.g., Ace, 5, Queen)
+     */
+    public Card(Suit s, Rank r) {
+        suit = s;
+        rank = r;
+    }
 
     public Rank getRank() {
         return rank;
@@ -51,5 +61,10 @@ public class Card {
         JACK,
         QUEEN,
         KING
+    }
+    
+    @Override
+    public String toString() {
+        return "[" + suit + ", " + rank +"]";
     }
 }
--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -1,10 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package blackjack;
 
+import blackjack.Card.Rank;
+import blackjack.Card.Suit;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  *
@@ -12,13 +12,41 @@ import java.util.List;
  */
 public class Dealer {
     
-    private List<Card> deck;
+    private Stack<Card> deck;
+    
+    /**
+     * Default constructor.
+     */
+    public Dealer() {
+        
+        // initialize deck of cards
+        deck = new Stack<Card>();
+        
+        // add cards to deck
+        for (Suit s : Card.Suit.values()) {
+            for (Rank r : Card.Rank.values()) {
+                Card newCard = new Card(s, r);
+                deck.push(newCard);
+            }
+        }
+        
+        // <testing>
+        //printDeck();
+        // </testing>
+        
+        // shuffle cards
+        shuffle(deck);
+        
+        // <testing>
+        //printDeck();
+        // </testing>
+    }
 
-    public List<Card> getDeck() {
+    public Stack<Card> getDeck() {
         return deck;
     }
 
-    public void setDeck(List<Card> deck) {
+    public void setDeck(Stack<Card> deck) {
         this.deck = deck;
     }
     
@@ -27,12 +55,20 @@ public class Dealer {
         return null;
     }
     
-    public void shuffle() {
-        //TODO: implement
+    public final void shuffle(List<Card> deck) {
+        Collections.shuffle(deck);
        
     }
     
     public void getOutcome() {
         //TODO: Implement
+    }
+    
+    // helper function - can remove after finished testing
+    public void printDeck() {
+        for (Card c : deck) {
+            System.out.print(c + " ");
+        }
+        System.out.println("");
     }
 }
